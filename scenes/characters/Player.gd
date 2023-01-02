d15fc1c23e9d1dd609c724f2f792b4714ddbb62f
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2023-01-02 12:49:39 -0700

    Added collisions for tilemap
    working on player gravity and jump

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -5,6 +5,8 @@ extends KinematicBody2D
 
 # Exported Vars
 export var speed = 20000
+export var player_gravity = 1
+export var jump_impulse = 1
 
 
 
@@ -17,8 +19,10 @@ var velocity = Vector2.ZERO
 
 #Functions
 func _physics_process(delta):
-	velocity = Vector2.ZERO
+	velocity.x = 0
+	velocity.y += player_gravity
 	check_input()
+	
 	velocity = move_and_slide(velocity * speed * delta)
 
 func check_input():
@@ -26,5 +30,13 @@ func check_input():
 		velocity.x -= 1
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
+	print(is_on_floor())
+	if Input.is_action_just_pressed("jump"):
+		print('j')
+		velocity.y = -jump_impulse
+
+func apply_gravity():
+	velocity.y += player_gravity
+	velocity.y = min(velocity.y, 300)
 	
 
