8062d5a73b611fbee37396539b070192050bb5bf
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2023-01-02 13:03:39 -0700

    Update Player.gd
    Coded player jump and gravity. Changed movement to not rely on delta

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -4,9 +4,9 @@ extends KinematicBody2D
 
 
 # Exported Vars
-export var speed = 20000
-export var player_gravity = 1
-export var jump_impulse = 1
+export var speed = 200
+export var player_gravity = 20
+export var jump_impulse = 500
 
 
 
@@ -20,23 +20,21 @@ var velocity = Vector2.ZERO
 #Functions
 func _physics_process(delta):
 	velocity.x = 0
-	velocity.y += player_gravity
+	apply_gravity()
 	check_input()
-	
-	velocity = move_and_slide(velocity * speed * delta)
+	velocity.x *= speed
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func check_input():
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-	print(is_on_floor())
-	if Input.is_action_just_pressed("jump"):
-		print('j')
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_impulse
 
 func apply_gravity():
 	velocity.y += player_gravity
-	velocity.y = min(velocity.y, 300)
+	velocity.y = min(velocity.y, 30000)
 	
 
