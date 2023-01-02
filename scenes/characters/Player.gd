a34d295d5067a9027d2d3b287b26d04197ef2971
Jadale18 <95486525+Jadale18@users.noreply.github.com>
2023-01-02 15:44:26 -0700

    Player Anims
    Added AnimatedSprite node to Player. Resized collsion shape. Coded animations for 'idle', 'walk', and 'fall' states

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -22,13 +22,24 @@ func _physics_process(delta):
 	velocity.x = 0
 	apply_gravity()
 	check_input()
+	if velocity.x == 0:
+		$AnimatedSprite.speed_scale = 0.5
+		$AnimatedSprite.play("Idle")
+	else:
+		$AnimatedSprite.speed_scale = 1.3
+		$AnimatedSprite.play("Walk")
+	
+	if not is_on_floor():
+		$AnimatedSprite.play("Fall")
 	velocity.x *= speed
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func check_input():
 	if Input.is_action_pressed('left'):
+		$AnimatedSprite.flip_h = false
 		velocity.x -= 1
 	if Input.is_action_pressed("right"):
+		$AnimatedSprite.flip_h = true
 		velocity.x += 1
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_impulse
